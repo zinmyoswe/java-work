ce10646226d5dce80588abf563c92ad02bb24e96
ZIN MYO <overmidnight12@gmail.com>
2019-06-12 12:54:44 -0700

    my changes

---
+++ java_useful/src/java_useful/array2.java
@@ -0,0 +1,12 @@
+package java_useful;
+
+public class array2 {
+	public static void main(String args[]) {
+		int[][] mynum = {{1,2,3,4},{5,6,7,8}};
+		for(int i =0; i < mynum.length; i++) {
+			for(int j=0; j< mynum[i].length; ++j) {
+				System.out.println(mynum[i][j]);
+			}
+		}
+	}
+}
