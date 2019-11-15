0164553d8b9fda9e8c8486283787c9715d5afc22
ZIN MYO <overmidnight12@gmail.com>
2019-11-14 23:38:40 -0800

    my changes

---
+++ java_useful/src/java_useful/java_file.java
@@ -0,0 +1,18 @@
+package java_useful;
+import java.io.File;
+
+public class java_file {
+	public static void main(String[] args) {
+		File myObj = new File("D:\\Tutorial2\\file1.txt");
+		if(myObj.exists()) {
+			System.out.println("File name :"+ myObj.getName());
+			System.out.println("Absolute path :"+ myObj.getAbsolutePath());
+			System.out.println("Writeable :"+ myObj.canWrite());
+			System.out.println("Readable :"+ myObj.canRead());
+			System.out.println("File size in bytes :"+ myObj.length());
+		}
+		else {
+			System.out.println("The file does not exist");
+		}
+	}
+}
